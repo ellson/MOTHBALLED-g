b97bd95c13c482ad7448ddddc8d1aa3b17ba509b
John Ellson <ellson@research.att.com>
2015-07-13 21:36:41 -0400

    not sure about this...

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,96 +2,179 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-unsigned char characters[] = {
-     0,  1,  1,  1,  1,  1,  1,  1,  /* NUL SOH STX ETX EOT ENQ ACK BEL */
-     1,  2,  2,  1,  1,  2,  1,  1,  /*  BS TAB  LF  VT  FF  CR  SO  SI */
-     1,  1,  1,  1,  1,  1,  1,  1,  /* DLE DC1 DC2 DC3 DC4 NAK STN ETB */
-     1,  1,  1,  1,  1,  1,  1,  1,  /* CAN  EM SUB ESC  FS  GS  RS  US */
-     2,  1,  3,  4,  1,  1,  1,  5,  /* SPC  !   "   #   $   %   &   '  */
-     6,  7,  8,  1,  1,  1,  1,  9,  /*  (   )   *   +   ,   -   .   /  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  0   1   2   3   4   5   6   7  */
-     1,  1, 10, 11, 12, 13, 14,  1,  /*  8   9   :   ;   <   =   >   ?  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  @   A   B   C   D   E   F   G  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  H   I   J   K   L   M   N   O  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  P   Q   R   S   T   U   V   W  */
-     1,  1,  1, 15, 16, 17,  1,  1,  /*  X   Y   Z   [   \   ]   ^   _  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  `   a   b   c   d   e   f   g  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  h   i   j   k   l   m   n   o  */
-     1,  1,  1,  1,  1,  1,  1,  1,  /*  p   q   r   s   t   u   v   w  */
-     1,  1,  1, 18,  1, 19, 20,  1,  /*  x   y   z   {   |   }   ~  DEL */
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1,
-     1,  1,  1,  1,  1,  1,  1,  1
+typedef enum {
+    NLL, ABC, WSP, DQT,
+    OCT, SQT, LPN, RPN,
+    AST, FSL, CLN, SCN,
+    LAN, EQL, RAN, LBR,
+    BSL, RBR, LBE, RBE,
+    TLD
+} tok_t;
+ 
+tok_t char2tok[] = {
+   NLL, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /* NUL SOH STX ETX EOT ENQ ACK BEL */
+   ABC, WSP, WSP, ABC, ABC, WSP, ABC, ABC,  /*  BS TAB  LF  VT  FF  CR  SO  SI */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /* DLE DC1 DC2 DC3 DC4 NAK STN ETB */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /* CAN  EM SUB ESC  FS  GS  RS  US */
+   WSP, ABC, DQT, OCT, ABC, ABC, ABC, SQT,  /* SPC  !   "   #   $   %   &   '  */
+   LPN, RPN, AST, ABC, ABC, ABC, ABC, FSL,  /*  (   )   *   +   ,   -   .   /  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  0   1   2   3   4   5   6   7  */
+   ABC, ABC, CLN, SCN, LAN, EQL, RAN, ABC,  /*  8   9   :   ;   <   =   >   ?  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  @   A   B   C   D   E   F   G  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  H   I   J   K   L   M   N   O  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  P   Q   R   S   T   U   V   W  */
+   ABC, ABC, ABC, LBR, BSL, RBR, ABC, ABC,  /*  X   Y   Z   [   \   ]   ^   _  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  `   a   b   c   d   e   f   g  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  h   i   j   k   l   m   n   o  */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,  /*  p   q   r   s   t   u   v   w  */
+   ABC, ABC, ABC, LBE, ABC, RBE, TLD, ABC,  /*  x   y   z   {   |   }   ~  DEL */
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC,
+   ABC, ABC, ABC, ABC, ABC, ABC, ABC, ABC
 };
 
-unsigned char* char_null(unsigned char c){exit(0);}
-unsigned char* char_not_special(unsigned char c){return((unsigned char*)"not special");}
-unsigned char* char_whitespace(unsigned char c){return((unsigned char*)"whitespace");}
-unsigned char* char_double_quote(unsigned char c){return((unsigned char*)"double_quote");}
-unsigned char* char_octothorpe(unsigned char c){return((unsigned char*)"octothorpe");}
-unsigned char* char_single_quote(unsigned char c){return((unsigned char*)"single quote");}
-unsigned char* char_left_paren(unsigned char c){return((unsigned char*)"left paren");}
-unsigned char* char_right_paren(unsigned char c){return((unsigned char*)"right paren");}
-unsigned char* char_asterisk(unsigned char c){return((unsigned char*)"asterisk");}
-unsigned char* char_forward_slash(unsigned char c){return((unsigned char*)"forward slash");}
-unsigned char* char_colon(unsigned char c){return((unsigned char*)"colon");}
-unsigned char* char_semicolon(unsigned char c){return((unsigned char*)"semicolon");}
-unsigned char* char_lessthan(unsigned char c){return((unsigned char*)"lessthan");}
-unsigned char* char_equals(unsigned char c){return((unsigned char*)"equals");}
-unsigned char* char_greaterthan(unsigned char c){return((unsigned char*)"greaterthan");}
-unsigned char* char_left_bracket(unsigned char c){return((unsigned char*)"left bracket");}
-unsigned char* char_back_slash(unsigned char c){return((unsigned char*)"backslash");}
-unsigned char* char_right_bracket(unsigned char c){return((unsigned char*)"right bracket");}
-unsigned char* char_left_brace(unsigned char c){return((unsigned char*)"left brace");}
-unsigned char* char_right_brace(unsigned char c){return((unsigned char*)"right brace");}
-unsigned char* char_tilde(unsigned char c){return((unsigned char*)"tilde");}
+typedef enum {
+    BEG,
+    APPEND,
+    TERM_POP,
+    TERM,
+    TERM_AND_REUSE
+} state_t;
+ 
+void char_null (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"null\n");
+    exit(0);
+}
 
-typedef unsigned char* (*charfunc)(unsigned char);
+void char_not_special (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"not special\n");
+}
+
+void char_whitespace (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"whitespace\n");
+}
+
+void char_double_quote (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"double_quote\n");
+}
+
+void char_octothorpe (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"octothorpe\n");
+}
+
+void char_single_quote (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"single quote\n");
+}
+
+void char_left_paren (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"left paren\n");
+}
+
+void char_right_paren (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"right paren\n");
+}
+
+void char_asterisk (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"asterisk\n");
+}
+
+void char_forward_slash (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"forward slash\n");
+}
+
+void char_colon (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"colon\n");
+}
+
+void char_semicolon (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"semicolon\n");
+}
+
+void char_lessthan (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"lessthan\n");
+}
+
+void char_equals (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"equals\n");
+}
+
+void char_greaterthan (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"greaterthan\n");
+}
+
+void char_left_bracket (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"left bracket\n");
+}
+
+void char_back_slash (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"backslash\n");
+}
+
+void char_right_bracket (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"right bracket\n");
+}
+
+void char_left_brace (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"left brace\n");
+}
+
+void char_right_brace (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"right brace\n");
+}
+
+void char_tilde (state_t *state, tok_t *beg_tok) {
+    fprintf(stdout,"tilde\n");
+}
+
+typedef void (*charfunc)(state_t *state, tok_t *beg_tok);
 charfunc charproc[]={
-    char_null,   	/*  0 */
-    char_not_special,	/*  1 */
-    char_whitespace,   	/*  2 */
-    char_double_quote,	/*  3 */
-    char_octothorpe,	/*  4 */
-    char_single_quote,	/*  5 */
-    char_left_paren,	/*  6 */
-    char_right_paren,	/*  7 */
-    char_asterisk,	/*  8 */
-    char_forward_slash,	/*  9 */
-    char_colon,		/* 10 */
-    char_semicolon,	/* 11 */
-    char_lessthan,	/* 12 */
-    char_equals,	/* 13 */
-    char_greaterthan,	/* 14 */
-    char_left_bracket,	/* 15 */
-    char_back_slash,	/* 16 */
-    char_right_bracket,	/* 18 */
-    char_left_brace,	/* 19 */
-    char_right_brace,	/* 20 */
-    char_tilde		/* 21 */
+    char_null,   	/* NLL */
+    char_not_special,	/* ABC */
+    char_whitespace,   	/* WSP */
+    char_double_quote,	/* DQT */
+    char_octothorpe,	/* OCT */
+    char_single_quote,	/* SQT */
+    char_left_paren,	/* LPN */
+    char_right_paren,	/* RPN */
+    char_asterisk,	/* AST */
+    char_forward_slash,	/* FSL */
+    char_colon,		/* CLN */
+    char_semicolon,	/* SCN */
+    char_lessthan,	/* LAN */
+    char_equals,	/* EQL */
+    char_greaterthan,	/* RAN */
+    char_left_bracket,	/* LBR */
+    char_back_slash,	/* BSL */
+    char_right_bracket,	/* RBR */
+    char_left_brace,	/* LBE */
+    char_right_brace,	/* RBE */
+    char_tilde		/* TLD */
 };
-    
+
 int main (int argc, char *argv[]) {
     unsigned char *test=(unsigned char*)"<a b c>";
     unsigned char *p;
+    state_t state;
+    tok_t beg_tok;
 
     p = test;
     while (true) {
         unsigned char c = *p++;
-        fprintf(stdout,"%s\n",charproc[characters[c]](c));
+        tok_t this = char2tok[c];
+        charproc[this](&state, &beg_tok);
     }
 }
