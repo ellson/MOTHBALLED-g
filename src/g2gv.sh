4eee98b1f677a7d067a6fcfb74cd30b63d7b099c
John Ellson <john.ellson@comcast.net>
2015-08-08 12:52:46 -0400

    add index.html to collect all docs into one place

--- src/g2gv.sh
+++ src/g2gv.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 echo "strict digraph { ordering=out"
-while read op t h x x; do
+
+sed '{s/</\n< /g;s/>/\n>/g;s/\[/\n \[ /g;s/\]/\n \]/g;s/{/\n { /g;s/}/\n }/g}' |
+while read op t h x; do
     if test "$op" = "<"; then
-	echo "  \"$t\" -> \"$h\""
+	if test "$t" = "="; then
+	    t=$se
+        else
+	    se=$t
+        fi
+        echo "  \"$t\" -> \"$h\""
     fi
 done
+
 echo "}"
