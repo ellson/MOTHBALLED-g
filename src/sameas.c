19afde7fafb41ce42745a5ccc5b1c18f057517b9
John Ellson <john.ellson@comcast.net>
2015-08-15 09:03:56 -0400

    wip - sameas

--- src/sameas.c
+++ src/sameas.c
@@ -8,14 +8,14 @@
 #include "emit.h"
 
 
-static void sameas_r(container_context_t *CC, elem_t *list, elem_t *oldlist, elem_t *newlist) {
-    elem_t *elem, *old, *new;
+// flatten list into new list with any EQL elements substituted from oldlist
+static void sameas_r(container_context_t *CC, elem_t *list, elem_t **nextold, elem_t *newlist) {
+    elem_t *elem, *new;
     state_t si;
 
     assert (list->type == (char)LISTELEM);
 
     elem = list->u.list.first;
-    old = oldlist->u.list.first;
     while(elem) {
         si = (state_t)elem->state;
 	switch (si) {
@@ -25,16 +25,17 @@ static void sameas_r(container_context_t *CC, elem_t *list, elem_t *oldlist, ele
 	    new = ref_list(elem->state, elem);
 	    append_list(newlist, new); 
 	    elem = elem->next;
-	    if (old) {
-	        old = old->next;
+	    if (*nextold) {                 // doesn't matter if old is shorter
+					    // ... as long as no forther substitutions are needed
+	        *nextold = (*nextold)->next;
             }
 	    continue;
 	case EQL:
-	    if (old) {
-	        new = ref_list(old->state, old);
+	    if (*nextold) {
+	        new = ref_list((*nextold)->state, *nextold);
 	        append_list(newlist, new); 
 	        elem = elem->next;
-	        old = old->next;
+	        *nextold = (*nextold)->next;
             }
 	    else {
 		emit_error(CC->context, si, "No corresponding object found for same-as substitution");
@@ -43,31 +44,30 @@ static void sameas_r(container_context_t *CC, elem_t *list, elem_t *oldlist, ele
         default:
 	    break;
 	}
-	sameas_r(CC, elem, oldlist, newlist);   // recurse
+	sameas_r(CC, elem, nextold, newlist);   // recurse
 	elem = elem->next;
-	if (old) {
-	    old = old->next;
-        }
     }
 }
 
-//     flatten SUBJECT into a list of LEGS (or ENDPOINT or NODEID ?)
-//     compare flattened list with prev_subject (already flattened)
-//     substitue sameends (or error if no leg exists at index of EQL)
+//     flatten SUBJECT into a list of ENDPOINT or NODEID
+//     compare new flattened list with old (prev subject, already flattened)
+//     substitue EQL in new from corresponding member of old (or error if old not available)
 //     free prev_subject
-//     save new (fully substitued and flattened) subject as prev_subject
+//     save new (fully flattened and EQL substituted) list as prev_subject
 success_t sameas(container_context_t *CC, elem_t *list) {
     success_t rc;
-    elem_t *elem, *newlist, *oldlist;;
-    elem_t subject = {0};
+    elem_t *newlist, *oldlist, *nextold;
+    elem_t flattened_subject = {0};
 
-    newlist = &subject;
+    newlist = &flattened_subject;
     oldlist = &(CC->prev_subject);
+    nextold = oldlist->u.list.first;
 
-    sameas_r(CC, list, oldlist, newlist);
+    // flatten list into new list with any EQL elements substituted from oldlist
+    sameas_r(CC, list, &nextold, newlist);
 
-    free_list(list);
-    free_list(oldlist);   // update prev_subject for same_end substitution
+    free_list(list);        // free original tree ( although refs are retained in other lists )
+    free_list(oldlist);     // update prev_subject for same_end substitution
     *oldlist = *newlist;    // transfers all refs ... newlist will be out of scope shortly
 
 putc ('\n', stdout);
@@ -77,21 +77,3 @@ putc ('\n', stdout);
     rc = SUCCESS;
     return rc;
 }
-
-
-
-#if 0
-            if (bi == EQL) {
-                if (! sameend_elem) {
-                    emit_error(C, si, "No prior LEG found for sameend substitution in");
-                }
-//              elem = ref_list(si, elem);
-
-                elem = ref_list(si, sameend_elem);
-// FIXME can be multiple ENDPOINTS in a LEG, need a while here
-//                append_list(&branch, sameend_elem->u.list.first);
-            }
-            if (sameend_elem) {
-                sameend_elem = sameend_elem -> next;
-            }
-#endif
