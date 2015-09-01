2a0864e33a08c9c49200f0c9a8386bb89f5d027a
John Ellson <ellson@research.att.com>
2015-09-01 15:20:08 -0400

    minor notes

--- src/comm.c
+++ src/comm.c
@@ -3,7 +3,7 @@
 
 //
 //
-//  g_deamon:      user@host/session[]
+//  g_deamon:              ^/session[]
 //                          /stats[]
 //                          /graph{ a b c <a b> }
 //                          /net{
@@ -26,3 +26,21 @@
 // port_type:  server, client
 // poll_interval...
 // refresh_interval... 
+
+
+// short paths from graph:
+//          ^ => ^/net
+//          B => B/graph
+//
+// so:      ^/B/b   =>  ~/net/B/graph/b
+
+
+// no way from graph to refer to ~/stats  or ~/session   ??? is this reasonable?
+// 
+//   how does anyone read or write to net/  ?
+//   what do contained nets look like?
+// 
+//
+// Fram another host's net/ :
+//          B/graph/b
+//          B/session   
