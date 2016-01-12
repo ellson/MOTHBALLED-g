e29d161aa1b96c51f2c7fa0ccfac08fc51920708
John Ellson <ellson@research.att.com>
2016-01-12 14:28:51 -0500

    split out ENDPOINTSET expansion

--- src/dispatch.c
+++ src/dispatch.c
@@ -34,98 +34,6 @@
 //              is repeated, sending it though the nameless channel to
 //              their parent, which applies the same rule:
 
-// this function expands ENDPOINTSETs
-static void je_expand_r(context_t *C, elem_t *epset, elem_t *leg, elem_t *edges)
-{
-    elem_t newedge = { 0 };
-    elem_t *epsetlast, *ep, *new;
-
-    if (leg) {
-        ep = leg->u.list.first;
-        while(ep) {
-
-            // keep track of last
-            epsetlast = epset->u.list.last;
-
-            // append the next ep for this leg
-            new = ref_list(C, ep);    // FIXME - this is a temp list - can we do this without refcounting? Would it save much?
-            append_list(epset, new);
-            
-            // recursively process the rest of the legs
-            je_expand_r(C, epset, leg->next, edges);
-
-            // remove this leg's new ep from epset
-            epset->u.list.last = epsetlast;
-            free_list(C, new);        // FIXME - this is a temp list - can we do this without refcounting?
-
-            // and iterate to next ep for this leg
-            ep = ep->next;
-        }
-    }
-    else {
-        // if no more legs, then we can create a new edge with the current epset
-        newedge.state = EDGE;
-        ep = epset->u.list.first;
-        while (ep) {
-            new = ref_list(C, ep);
-            append_list(&newedge, new);
-            ep = ep->next;
-        }
-        // and append the new simplified edge to the result
-        new = move_list(C, &newedge);
-        append_list(edges, new);
-    }
-}
-
-// this function expands EDGEs
-static void je_expand(context_t *C, elem_t *elem, elem_t *nodes, elem_t *edges)
-{
-    elem_t *leg, *epset, *ep, *new;
-    elem_t newlist = { 0 };
-    elem_t newepset = { 0 };
-
-    assert(elem);
-
-    // build a leg list with endpointsets for each leg
-    leg = elem->u.list.first;
-    assert((state_t)leg->state == LEG);
-    while (leg) {
-        epset = leg->u.list.first;
-        new = ref_list(C, epset);
-        if ((state_t)epset->state == ENDPOINT) { // put singletons into lists too
-            newepset.state = ENDPOINTSET;
-            append_list(&newepset, new);
-            new = move_list(C, &newepset);
-        }
-        append_list(&newlist, new);
-
-        // induce all sibling nodes....
-        assert((state_t)new->state == ENDPOINTSET);
-        ep = new->u.list.first;
-        while(ep) {
-            assert((state_t)ep->state == ENDPOINT);                    
-            switch (ep->u.list.first->state) {
-            case SIBLING:
-                new = ref_list(C, ep->u.list.first);
-                append_list(nodes, new);
-                // FIXME - induce CHILDren in this node's container
-                break;
-            case COUSIN:
-                // FIXME - route to ancestors
-                break;
-            default:
-                assert(0);  // shouldn't happen
-                break;
-            }
-            ep = ep->next;
-        }
-        leg = leg->next;
-    }
-    // now recursively generate all combinations of ENDPOINTS in LEGS, and append new simplified EDGEs to edges
-    je_expand_r(C, &newepset, newlist.u.list.first, edges);
-    free_list(C, &newlist);
-}
-
 // this function expands OBJECT_LISTS of NODES or EDGES, and then expands ENPOINTSETS in EDGES
 static void je_dispatch_r(context_t * C, elem_t * list, elem_t * attributes, elem_t * nodes, elem_t * edges)
 {
@@ -164,7 +72,7 @@ static void je_dispatch_r(context_t * C, elem_t * list, elem_t * attributes, ele
             append_list(nodes, new);
             break;
         case EDGE:
-            je_expand(C, elem, nodes, edges);
+            je_expand_edge(C, elem, nodes, edges);
             break;
         default:
             break;
