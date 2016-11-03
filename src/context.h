4f4494acc6909eb14bee081716b522524d2514fc
John Ellson <john.ellson@comcast.net>
2016-11-03 02:07:44 -0400

    wip - getting rid of context.h - little steps!

--- src/context.h
+++ src/context.h
@@ -59,29 +59,6 @@ typedef struct {               // parse context
     style_t style;             // spacing style in emitted outputs
     ikea_store_t *ikea_store;  // persistency
     ikea_box_t *namehash_buckets[64];
-
-#if 0
-// FIXME -- session context
-
-    char needstats;            // flag set if -s on command line
-    char *progname;            // name of program
-    char *username;            // set by first call to g_session
-    char *hostname;          
-    char *osname;
-    char *osrelease;
-    char *osmachine;
-
-#if defined(HAVE_CLOCK_GETTIME)
-    // Y2038-unsafe struct - but should be ok for uptime
-    // ref: https://sourceware.org/glibc/wiki/Y2038ProofnessDesign
-    struct timespec uptime;     // time with subsec resolution since boot, used as the base for runtime calculations
-#else
-    // Y2038-unsafe struct - but should be ok for uptime
-    // ref: https://sourceware.org/glibc/wiki/Y2038ProofnessDesign
-    struct timeval uptime;      // time with subsec resolution since boot, used as the base for runtime calculations
-#endif
-    pid_t pid;
-#endif
 } GRAPH_t;
 
 typedef struct {                // container_context
