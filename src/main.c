4c88e79055e5550a51a94d3f5289a406d01fc7ec
John Ellson <john.ellson@comcast.net>
2016-11-20 10:01:41 -0500

    wip

--- src/main.c
+++ src/main.c
@@ -30,9 +30,24 @@ static void add_input(int thread, char inputtype, char *input)
 
 static void usage(char *prog) {
     fprintf(stderr,"\
-Usage: %s [-h?V]\n\
-       %s [[-p] [[-f <file> ... ] | [-s <socket>] | [-e <string>]]]\n\
+Usage: %s [-Vh?]\n\
+       %s [-D<dom>][-o<file>][-T<fmt>][[-p][[-f<file>...]|[-s<skt>]|[-e<str>]]]\n\n\
 ", prog, prog);
+    fprintf(stderr,"\
+ -V          - Print version and exit\n\
+ -h          - Print this help text and exit\n\
+ -?          - Print this help text and exit\n\
+ -D<dom>     - Select DOM where: <dom> = 'g'  - one time graph (default)\n\
+                                 <dom> = 'G'  - persistent graph\n\
+                                 <dom> = 'c'  - document\n\
+                                 <dom> = 'f'  - filter\n\
+ -o<file>    - output to file (default stdout)\n\
+ -T<fmt>     - output format:  g, (more to come)\n\
+ -p          - start new parallel thread for following inputs\n\
+ -f<file>... - input from file(s).  Use '-' for stdin. (default stdin)\n\
+ -e<str>     - input from string\n\
+ -s<skt>     - input from socket\n\
+");
 }
 
 static void version(void) {
