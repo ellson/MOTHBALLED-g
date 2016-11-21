682c1ce7a97aa3214b950581780c9ebbe38ba2a1
John Ellson <ellson@research.att.com>
2016-11-20 22:39:12 -0500

    wip - new main()

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 #include <signal.h>
 
 // #include "libg_session.h"
@@ -26,74 +27,84 @@ static void intr(int s)
     exit (EXIT_FAILURE);
 }
 
-// FIXME - add supports for (RESTful??) https access
-//  e.g      -s<skt>   - input from socket\n\
-//           [-p][[-f<fn>...]|[-s<skt>]|[-e<str>]]]\n\n\
-
 static void usage(char *prog) {
-    fprintf(stderr,"\
-Usage: %s [-Vh?]\n\
-       %s [<DOM_and_output>][<input_source_and_thread>]*\n\n\
-", prog, prog);
-fprintf(stderr,"\
-where: <DOM_and_output>  is:\n\
-       [-D<dom>][-o<fn>][-T<fmt>]\n\n\
-  and: <input_source>\n\
-       [-p][[-f<fn>...]|[-e<str>]]]\n\n\
- -V        - Print version and exit\n\
+    fprintf(stderr,"\n\
+Usage: %s [-vh]\n\
+       %s [<source>]* [<dest>]\n\
+\n\
+where <source> is:\n\
+       [-p] [[-e<str>] | [-f<fn>...] | [-s<skt>]]\n\
+\n\
+and where <dest> is:\n\
+       [-D<dom>][-T<fmt>][K<lay>][-o<fn>\n\
+\n\
+ -v        - Print version and exit\n\
  -h        - Print this help text and exit\n\
- -?        - Print this help text and exit\n\
+\n\
+ -p        - start new parallel thread for following inputs\n\
+ -e<str>   - input from string\n\
+ -f<fn>... - input from file(s).  Use '-' for stdin. (default stdin)\n\
+ -s<skt>   - input from socket\n\
+\n\
  -D<dom>   - Select DOM type:\n\
                <dom> = 'g'  - one time graph (default)\n\
                <dom> = 'G'  - persistent graph\n\
-               <dom> = 'd'  - document\n\
+               <dom> = 'd'  - document (retains ordering and duplicates)\n\
                <dom> = '0'  - null dom for: translator, prettyprinter\n\
+ -T<fmt>   - output format:  g (default),  or any supported by dot\n\
+ -K<lay>   - output layout:  none (default for -Tg or -Tgv),\n\
+                             or any layout supported by dot\n\
  -o<fn>    - output to file (default stdout)\n\
- -T<fmt>   - output format:  g, (more to come)\n\
- -t<tab>   - tabsize to use for output nesting (default 0)\n\
- -p        - start new parallel thread for following inputs\n\
- -f<fn>... - input from file(s).  Use '-' for stdin. (default stdin)\n\
- -e<str>   - input from string\n\
-");
+\n", prog, prog);
 }
 
 static void version(void) {
-    fprintf(stderr,"Version: %s-%s\n", PACKAGE_NAME, PACKAGE_VERSION);
+    fprintf(stderr,"%s version %s\n", PACKAGE_NAME, PACKAGE_VERSION);
 }
 
-// FIXME - move to libg_process. public api
+/**************** FIXME - move to libg_process public api ***/
+ 
 static int
-process_input(int thread, char inputsource, char *input, long sequence)
+process_input(int thread, char type, char *input, long sequence)
 {
-    printf("%d %c: %s\n", thread, inputsource, input);
+    printf("inp: -%c\"%s\"`%ld par: %d\n", type, input, sequence, thread);
     return 0;
 }
 
-// FIXME - move to libg_process. public api
-static void
-process_start_threads(void)
+static int
+process_output(char *output, char *layout, char* format)
 {
-    printf("go Go GO\n");
+    printf("out: -o\"%s\" -K%s -T%s\n", output, layout, format);
+    return 0;
 }
+
+static void
+process_dom_start(char dom)
+{
+    printf("dom: -D%c\n", dom);
+}
+/***********************************************************/
 
 int main(int argc, char *argv[])
 {
     int opt, optnum, rc;
 
-// defaults
+    char multiopt = 'f';
+    int inpcnt = 0;
+    int outcnt = 0;
+
+// initial source values (defaults)
     int thread = 0;
-    char inputsource = 'f';
-    char dom = 'g';
-    char *outputfile = "-";
-    char outputtype = 'g';
-    int outputtab = 0;
     long sequence = 0;
+
+// initial dest values (defaults)
+    char dom = 'g';
+    char *layout = "";
+    char *format = "g";
 
     signal(SIGINT, intr);
 
-// FIXME - reserce -s for socket ((is https a socket??)
-
-    while ((opt = getopt(argc, argv, "-fepD:t:?hV")) != -1) {
+    while ((opt = getopt(argc, argv, "-efspK:T:D:ohv")) != -1) {
         if (optarg) {
             optnum = atoi(optarg);
         } else {
@@ -101,15 +112,33 @@ int main(int argc, char *argv[])
         }
         switch (opt) {
         case 1: 
-            rc = process_input(thread, inputsource, optarg, sequence++);
-            if (rc) {
-                exit(EXIT_FAILURE);
+            switch (multiopt) {
+            case 's':
+            case 'f':  
+            case 'e':   
+                rc = process_input(thread, multiopt, optarg, sequence++);
+                if (rc) {
+                    exit(EXIT_FAILURE);
+                }
+                inpcnt++;
+                break;
+            case 'o':    
+                rc = process_output(optarg, layout, format);
+                if (rc) {
+                    exit(EXIT_FAILURE);
+                }
+                outcnt++;
+                break;
+            default:
+                assert(0);
+                break;
             }
             break;
-// FIXME - reserve:  case 's':
+        case 's':
         case 'f':  
         case 'e':   
-            inputsource = opt;
+        case 'o':    
+            multiopt = opt;
             break;
         case 'p':    
             thread++;
@@ -120,29 +149,38 @@ int main(int argc, char *argv[])
             case 'G':
             case 'd':
             case '0':
+                dom = *optarg;
                 break;
             default:
-                usage(argv[0]);
+
+                fprintf(stderr,"%s: invalid argument to -D -- \'%c\'\n",
+                        argv[0], *optarg);
                 exit(EXIT_FAILURE);
                 break;
             }
-        case 'V':    
+            break;
+        case 'v':    
             version();
             exit(EXIT_SUCCESS);
             break;
         case 'h':
-        case '?':
             usage(argv[0]);
             exit(EXIT_SUCCESS);
             break;
+        case '?':
         default:
-            usage(argv[0]);
             exit(EXIT_FAILURE);
             break;
         }
     }
 
-    process_start_threads();
+    if (!inpcnt) {
+        (void)process_input(thread, 'f', "-", sequence++);
+    }
+    if (!outcnt) {
+        (void)process_output("-", "", "g");
+    }
+    process_dom_start(dom);
 
     exit(EXIT_SUCCESS);
 }
