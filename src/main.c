aa07628f88d6ab0c8c2c7447788076ccb20f32d8
John Ellson <ellson@research.att.com>
2016-11-20 19:28:23 -0500

    wip

--- src/main.c
+++ src/main.c
@@ -26,6 +26,10 @@ static void intr(int s)
     exit (EXIT_FAILURE);
 }
 
+// FIXME - add supports for (RESTful??) https access
+//  e.g      -s<skt>   - input from socket\n\
+//           [-p][[-f<fn>...]|[-s<skt>]|[-e<str>]]]\n\n\
+
 static void usage(char *prog) {
     fprintf(stderr,"\
 Usage: %s [-Vh?]\n\
@@ -35,7 +39,7 @@ fprintf(stderr,"\
 where: <DOM_and_output>  is:\n\
        [-D<dom>][-o<fn>][-T<fmt>]\n\n\
   and: <input_source>\n\
-       [-p][[-f<fn>...]|[-s<skt>]|[-e<str>]]]\n\n\
+       [-p][[-f<fn>...]|[-e<str>]]]\n\n\
  -V        - Print version and exit\n\
  -h        - Print this help text and exit\n\
  -?        - Print this help text and exit\n\
@@ -50,7 +54,6 @@ where: <DOM_and_output>  is:\n\
  -p        - start new parallel thread for following inputs\n\
  -f<fn>... - input from file(s).  Use '-' for stdin. (default stdin)\n\
  -e<str>   - input from string\n\
- -s<skt>   - input from socket\n\
 ");
 }
 
@@ -58,14 +61,24 @@ static void version(void) {
     fprintf(stderr,"Version: %s-%s\n", PACKAGE_NAME, PACKAGE_VERSION);
 }
 
-static void add_input(int thread, char inputsource, char *input)
+// FIXME - move to libg_process. public api
+static int
+process_input(int thread, char inputsource, char *input, long sequence)
 {
     printf("%d %c: %s\n", thread, inputsource, input);
+    return 0;
+}
+
+// FIXME - move to libg_process. public api
+static void
+process_start_threads(void)
+{
+    printf("go Go GO\n");
 }
 
 int main(int argc, char *argv[])
 {
-    int opt, optnum;
+    int opt, optnum, rc;
 
 // defaults
     int thread = 0;
@@ -74,10 +87,13 @@ int main(int argc, char *argv[])
     char *outputfile = "-";
     char outputtype = 'g';
     int outputtab = 0;
+    long sequence = 0;
 
     signal(SIGINT, intr);
 
-    while ((opt = getopt(argc, argv, "-fespD:t:?hV")) != -1) {
+// FIXME - reserce -s for socket ((is https a socket??)
+
+    while ((opt = getopt(argc, argv, "-fepD:t:?hV")) != -1) {
         if (optarg) {
             optnum = atoi(optarg);
         } else {
@@ -85,26 +101,31 @@ int main(int argc, char *argv[])
         }
         switch (opt) {
         case 1: 
-            add_input(thread, inputsource, optarg);
+            rc = process_input(thread, inputsource, optarg, sequence++);
+            if (rc) {
+                exit(EXIT_FAILURE);
+            }
             break;
+// FIXME - reserve:  case 's':
         case 'f':  
         case 'e':   
-        case 's':    
             inputsource = opt;
             break;
         case 'p':    
             thread++;
             break;
         case 'D':    
-#if 0
-            switch (optarg) {
+            switch (*optarg) {
             case 'g':
             case 'G':
             case 'd':
             case '0':
+                break;
+            default:
+                usage(argv[0]);
+                exit(EXIT_FAILURE);
+                break;
             }
-#endif
-            break;
         case 'V':    
             version();
             exit(EXIT_SUCCESS);
@@ -120,6 +141,8 @@ int main(int argc, char *argv[])
             break;
         }
     }
+
+    process_start_threads();
 
     exit(EXIT_SUCCESS);
 }
