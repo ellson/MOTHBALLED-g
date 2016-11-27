50f1266e2894cadf46c21b9780564f555b5b63ee
John Ellson <ellson@research.att.com>
2016-11-27 16:33:57 -0500

    wip

--- src/reduce.c
+++ src/reduce.c
@@ -7,6 +7,35 @@
 #include "thread.h"
 #include "reduce.h"
 
+/**
+ * merge VALUE for an ATTR which may have seen before
+ *                latest VALUE wins
+ *
+ * @param LIST 
+ * @param attr    - attr value 
+ * @param key     - key 
+ * @return        - key - not used
+ */
+static elem_t * merge_value(LIST_t *LIST, elem_t *attr, elem_t *key)
+{
+    THREAD_t *THREAD = (THREAD_t*)LIST;
+
+P(key);
+P(attr);
+    elem_t *oldattr = key->u.l.first;
+    elem_t *oldvalue = oldattr->u.l.next;
+    if (oldvalue) {
+        free_list(LIST, oldvalue);
+        oldattr->u.l.next = NULL;
+        key->u.l.last = key->u.l.first;
+        key->len = 1;
+    }
+    elem_t *value = attr->u.l.first->u.l.next;
+    if (value) {
+        append_transfer(key, value);
+    }
+    return key;
+}
 
 /**
  * merge ATTRIBUTEs for a NODE or EDGE which may have existing ATTRIBUTES
@@ -18,6 +47,7 @@
  */
 static elem_t * merge_attributes(LIST_t *LIST, elem_t *subject, elem_t *key)
 {
+#if 0
     elem_t *disambig, *attributes;
    
     disambig = subject->u.l.next;
@@ -41,13 +71,15 @@ static elem_t * merge_attributes(LIST_t *LIST, elem_t *subject, elem_t *key)
             attr = attr->u.l.next;
         }
     }
+#endif
     return key;
 }
 
 void reduce(CONTAINER_t * CONTAINER, elem_t *act, state_t verb)
 {
     THREAD_t * THREAD = CONTAINER->THREAD;
-    elem_t *subject, *disambig, *newsubj, *newnoun;
+    elem_t *subject, *disambig, *attributes, *attr;
+    elem_t *newsubj, *newnoun, *newattributes = NULL;
 
     assert(act);
     assert((state_t)act->state == ACT);
@@ -56,16 +88,44 @@ void reduce(CONTAINER_t * CONTAINER, elem_t *act, state_t verb)
     assert(subject);
     assert((state_t)subject->state == SUBJECT);
 
+//P(act);
+
     newsubj = new_list(LIST(), SUBJECT);
     newnoun = new_list(LIST(), NOUNS);
     append_transfer(newsubj, newnoun);
     append_addref(newnoun, subject->u.l.first);
     disambig = subject->u.l.next;
-    if (disambig) {
+    if (disambig && (state_t)disambig->state == DISAMBIG) {
         append_addref(newsubj, disambig);
+        attributes = disambig->u.l.next;
+    } else {
+        attributes = disambig;
+        disambig = NULL;
+    }
+    if (attributes) {
+        assert((state_t)attributes->state == ATTRIBUTES);
+        attr = attributes->u.l.first;
+        while (attr) {
+            elem_t *newattr = new_list(LIST(), ATTR);
+            append_addref(newattr, attr->u.l.first);
+            elem_t *value = attr->u.l.last;
+            if (value) {
+                append_transfer(newattr, value);
+            }
+//P(newattr);
+            newattributes = insert_item(LIST(), newattributes,
+                    newattr->u.l.first, merge_value, NULL); 
+//P(newattributes);
+
+            attr = attr->u.l.next;
+            free_list(LIST(), newattr);
+        }
+    }
+    if (newattributes) {
+        append_transfer(newsubj, newattributes);
     }
 
-//P(newsubj);
+P(newsubj);
 
     // FIXME - attributes need to be in a sorted tree
 
