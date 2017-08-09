54f3a05f909ca18f274466de9626de5b2360f268
John Ellson <ellson@research.att.com>
2017-08-09 17:04:18 -0400

    fix warning

--- src/ikea.c
+++ src/ikea.c
@@ -159,7 +159,7 @@ ikea_box_t *ikea_box_open( ikea_store_t * ikea_store, const char *appends_conten
     return ikea_box;
 }
 
-void ikea_box_append(ikea_box_t* ikea_box, const char *data, size_t data_len)
+void ikea_box_append(ikea_box_t* ikea_box, const unsigned char *data, size_t data_len)
 {
     if (fwrite(data, 1, data_len, ikea_box->fh) != data_len)
         FATAL("fwrite()");
--- src/ikea.h
+++ src/ikea.h
@@ -13,7 +13,7 @@ void ikea_store_restore ( ikea_store_t *ikea_store );
 void ikea_store_close ( ikea_store_t *ikea_store );
 
 ikea_box_t* ikea_box_open( ikea_store_t *ikea_store, const char *appends_content );
-void ikea_box_append( ikea_box_t* ikea_box, const char *data, size_t data_len );
+void ikea_box_append( ikea_box_t* ikea_box, const unsigned char *data, size_t data_len );
 void ikea_box_close( ikea_box_t* ikea_box, char *contenthash, int contenthashsz );
 
 void sslhash_list(uint64_t *hash, elem_t *list);
