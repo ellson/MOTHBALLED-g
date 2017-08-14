73d80ee3025128846cf58bfa77f98ac7aea0ca5c
John Ellson <ellson@research.att.com>
2017-08-14 17:17:49 -0400

    inline token_1()

--- src/parse.c
+++ src/parse.c
@@ -14,8 +14,6 @@
 
 static elem_t * merge_identifier(LIST_t *LIST, elem_t *identifier, elem_t *key);
 
-static void  token_1 (TOKEN_t * TOKEN, state_t si);
-
 /**
  * This parser recurses at two levels:
  *
@@ -91,8 +89,13 @@ success_t parse(CONTAINER_t * CONTAINER, elem_t *root, state_t si, unsigned char
     // Special character tokens
     if (si == TOKEN()->insi) {    // single character terminals matching
                                   //  state_machine expectation
-        bi = TOKEN()->insi;
-        token_1(TOKEN(),si);
+        bi = si;
+        if (++(TOKEN()->in) != TOKEN()->end) {
+            TOKEN()->insi = char2state[*(TOKEN()->in)];
+        }
+        else {
+            TOKEN()->insi = END;
+        }
         ei = TOKEN()->insi;
         goto done;
     }
@@ -323,31 +326,3 @@ static elem_t * merge_identifier(LIST_t *LIST, elem_t *identifier, elem_t *key)
     free_list(LIST, identifier);
     return key;
 }
-
-/**
- * scan input for a single character of the character state_t
- *
- * starts scanning at TOKEN->in
- * updates TOKEN->in to point to the next character after the accepted scan
- * updates TOKEN->insi to contain the state_t of the next character
- *
- * @param TOKEN context
- * @param si character class to be scanned for
- */
-
-static void token_1 (TOKEN_t * TOKEN, state_t si)
-{
-    unsigned char *in = TOKEN->in;
-
-    if (si == TOKEN->insi) {
-        in++;
-        if (in != TOKEN->end) {
-            TOKEN->insi = char2state[*in];
-        }
-        else {
-            TOKEN->insi = END;
-        }
-        TOKEN->in = in;
-    }
-}
-
