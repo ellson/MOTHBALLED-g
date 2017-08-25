8d5fc55a48cc6f25b2a5222729fa461776519e21
John Ellson <ellson@research.att.com>
2017-08-25 16:15:05 -0400

    WIP - adding GOM_t for Aunts

--- src/process.h
+++ src/process.h
@@ -10,6 +10,12 @@ extern "C" {
 #define TEN9 1000000000
 #define TEN3 1000
 
+// Graph Object Model
+typedef struct {
+    elem_t *nodes;    // tree of nodes (sorted, unique)
+    elem_t *edges;    // tree of edges (sorted, unique)
+} GOM_t;
+
 struct process_s {
     THREAD_t *THREAD;          // THREADs in this PROCESS
 
@@ -24,6 +30,11 @@ struct process_s {
     char *osname;
     char *osrelease;
     char *osmachine;
+
+    GOM_t MUM;       // Primary, in-memory graph  (one of the sisters, never explicitly named)
+    GOM_t SALLY;     // Aunt Sally,    Statistics  
+    GOM_t GUDRUN;    // Aunt Gudrun,   Grammar  (devine knowledge)
+
     uint64_t pid;
     uint64_t uptime;
     uint64_t uptime_nsec;
