958fe64c6bfc6b2e6aa1394ae6c2726ce210f1d2
John Ellson <ellson@research.att.com>
2017-08-30 09:57:47 -0400

    extract top container

--- gg.sh
+++ gg.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-g2gv() {
-    (echo "graph{"; cat $1; echo "}") | sed -e \
-    "s/_contenthash = \(.*\)$/label=<<table border=\"0\"><tr><td border=\"0\">\\\N<\/td><\/tr><tr><td border=\"0\" href=\"\1.svg\"><img src=\"\1.png\"\/><\/td><\/tr><\/table>>/" \
-    >${1%.g}.gv
-}
-
 g $* >/dev/null
 rm -rf rendering
 mkdir rendering
 cd rendering
 zcat ../g_snapshot.tgz | tar xf -
 
-for g in *; do
+echo "top.svg:" >Makefile
+for g in *.g; do
     i=${g%.g}
     echo -n "$i.svg $i.png: $i.gv" >>Makefile
+    echo "$i" >>is_content
     grep -h _contenthash $g | while read x x j; do
         echo -n " $j.svg $j.png" >>Makefile
+        echo "$j" >>is_contained
     done
     echo "" >>Makefile
 done
+
+sort -u is_content >is_content_t
+sort -u is_contained >is_contained_t
+diff is_content_t is_contained_t | while read dif top; do 
+    if test "$dif" == '<'; then
+	cat <<EOF >>Makefile
+top.svg: $top.svg
+ ln $top.svg top.svg
+EOF
+	break
+    fi
+done
+rm -f is_cont*
 
 cat <<EOF >>Makefile
 
